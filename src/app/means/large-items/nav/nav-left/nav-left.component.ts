275e61035c70b1200ecaf606642336e6713f2414
Leonardo Burgos Diaz <lbd_2014@hotmail.com>
2024-01-12 03:11:55 -0500

    Dashboard diseño

--- src/app/means/large-items/nav/nav-left/nav-left.component.ts
+++ src/app/means/large-items/nav/nav-left/nav-left.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-left',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavLeftComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  cerrarSesion(){
+    this.router.navigate(['login']);
+  }
 }
